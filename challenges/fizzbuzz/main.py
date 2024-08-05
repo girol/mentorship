149229dc291cf2d5fa621157b3ba9508d23c331a
André Luiz Girol <andregirol@gmail.com>
2024-08-05 10:00:15 -0300

    Adds the problem description

--- challenges/fizzbuzz/main.py
+++ challenges/fizzbuzz/main.py
@@ -1,3 +1,9 @@
+"""
+Escreva um programa em Python que que conte de 1 a 100. Porém, se o número da vez for
+divisível por 3, imprimir na tela a palavra "Fizz". Se for divisível por 5,
+imprimir "Buzz" e se for divisível tanto por 3 quanto por 5, imprimir "FizzBuzz"
+"""
+
 for i in range(1, 100):
     if i % 3 == 0 and i % 5 == 0:
         print("fizzbuzz")
