924ade683f92e27759361bb1f6ae71f1e5bd63a0
Aalim Jannaty <aalim.jannaty@gmail.com>
2024-08-14 13:58:43 -0300

    Create a programme to show greater number

---
+++ challenges/greater-number/main.py
@@ -0,0 +1,16 @@
+#Faça um Programa que peça dois números e imprima o maior deles.
+a = 400
+b = 200 
+if a > b:
+    print( "a is greater number then b")
+else :
+    print( "the number is smaller ")
+
+number1 = input( "please enter the number a =")
+number2 = input("please enter the second number b =")
+if number1 >= number2 :
+    print(number1)
+elif number2 >= number1:
+    print(number2)
+else:
+    print("0000000")
