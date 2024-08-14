0ec04d11858e7da049a1c5fb8c0a5a8ee90802b1
Aalim Jannaty <aalim.jannaty@gmail.com>
2024-08-14 11:35:09 -0300

    Created a program to distinguish male and female

---
+++ challenges/gender/main.py
@@ -0,0 +1,11 @@
+#Faça um Programa que verifique se uma letra digitada é "F" ou "M". Conforme a letra escrever:
+# F - Feminino, M - Masculino, Sexo Inválido.
+
+cha = input("type a letter" )
+if cha == 'f':
+    print("the person is female")
+
+elif cha == 'm':
+    print("the person is male")
+else:
+    print("not valid")
