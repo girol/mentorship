54b59b6213f9915343ab1392c95d4fbd76bd9b6c
André Luiz Girol <andregirol@gmail.com>
2024-08-20 14:29:40 -0300

    Improving the largest-smallest solution

--- challenges/largest-smalest/main.py
+++ challenges/largest-smalest/main.py
@@ -1,10 +1,23 @@
-#Faça um Programa que leia três números e mostre o maior e o menor deles
+# Faça um Programa que leia três números e mostre o maior e o menor deles
+
+numbers = []
 
 
-list = []
-num =  int(input("enter the range numbers: "))
-for i in range(1, num + 1):
-    ele = int(input("enter numbers"))
-    list.append(ele)
-print("largest number is ", max(list))
-print("Smallest numbe is ", min(list))
+def print_line(delimiter="="):
+    print(delimiter * 22)
+
+
+print_line(delimiter="#")
+print("Calculates the max and min number")
+print_line(delimiter="#")
+
+
+for i in range(1, 4):
+
+    number = input(f"Input the {i}° number: ")
+    numbers.append(int(number))
+
+print_line()
+print("largest number is: ", max(numbers))
+print("Smallest numbe is: ", min(numbers))
+print_line()
