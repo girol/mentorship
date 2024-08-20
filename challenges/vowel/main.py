bd8ee33acadf49d8f18a57ed381a17bcbaed8ae5
André Luiz Girol <andregirol@gmail.com>
2024-08-20 10:42:28 -0300

    Improving a little bit the main solution

--- challenges/vowel/main.py
+++ challenges/vowel/main.py
@@ -1,15 +1,19 @@
-#Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
-
-
-
-
-alpha = input("type a letter here")
+import sys
 
 VOWELS = ["a", "e", "i", "o", "u"]
+NUMBERS = ["1", "2", "3", "4", "5", "6", "8", "9", "0"]
+
+# Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
+alpha = input("type a letter: ")
+
+if len(alpha) > 1:
+    sys.exit("Invalid input: Type just one character")
+
+if alpha in NUMBERS:
+    sys.exit(f"Invalid input: {alpha} is a number")
 
 if alpha in VOWELS:
     print("the letter you typed is vowel")
 
 else:
     print("the letter you typed is consonant")
- 
