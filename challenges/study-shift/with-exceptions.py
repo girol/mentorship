d3e935066895a67b846ba4fc9ac750ae31e3825b
André Luiz Girol <andregirol@gmail.com>
2024-08-30 15:21:05 -0300

    Adiciona exemplo usando mapas e exceptions

---
+++ challenges/study-shift/with-exceptions.py
@@ -0,0 +1,31 @@
+"""
+Create a program that asks which shift you study.
+Ask to enter one of:
+ M - morning
+ V - Evening
+ N - Night.
+
+Print the message:
+"Good Morning!", "Good Afternoon!" or "Good Night!"
+or "Invalid Value!", as applicable.
+"""
+
+shift = (
+    input(
+        "Which shift do you study enter (M for morning, V for Evening, N for night)? \n>> "
+    )
+    .strip()
+    .upper()
+)
+
+# The same problem solved using
+# Dictionaries and an exception handler
+
+GREETINGS = {"M": "Good morning!", "V": "Good Evening!", "N": "Good night!"}
+
+try:
+    message = GREETINGS[shift]
+except KeyError:
+    print("Invalid Input")
+else:
+    print(message)
