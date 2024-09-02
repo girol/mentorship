e775dfe0e722542aa7e3eb6bb1b94e4e6c1eee46
Aalim Jannaty <aalim.jannaty@gmail.com>
2024-09-02 14:53:01 -0300

    create a program to enter two notes and callculate grades average show the possitions

--- challenges/grades/main.py
+++ challenges/grades/main.py
@@ -1,29 +1,35 @@
+
+
 """Write a program that reads the two partial grades obtained by a student in a subject over 
 the course of a semester, and calculates their average. 
 The assignment of grades follows the table below:"""
 
 
+def calculate_grade(grade1, grade2):
+    grade = grade1 + grade2 /2
 def print_line(delimiter="*"):
-   print(delimiter * 70)
-grade = float(input("\nEnter the mark for first Subject>>>>:"))
-grade1 = float(input("\nEnter the mark for the second Subject>>>:"))
-grade2 = float(input("\nEnter the mark for the third Subject>>>:"))
-grade3 = float(input("\nEnter the mark for th fourt  Subject>>>:"))
-grade4 = float(input("\nEnter the mark for the fifth Subject>>>:"))
-average = (grade + grade1+ grade2 + grade3 + grade4)/5
-print_line(delimiter="*")
-print("               Sub1    Sub2       Sub3     Sub4     sub5    T-av Grade \n")
+    print(delimiter * 50)
+print_line("*")
+grade1 = float(input("\nPlease enter the first grade from zero to ten>>:"))
+grade2 = float(input("\nPlease enter the second grade  from zero to ten>>:"))
+average = (grade1 + grade2) /2
 
-if 9 <= average <= 10 :
-    print(f" the mark is:  {grade}     {grade1}      {grade2}      {grade3}    {grade4}    {average}     :A     ")
-elif 7.5 <= average <= 9 :
-    print(f" the mark is: {grade}      {grade1}       {grade2}      {grade3}    {grade4}    {average}      :B    ")
-elif 6<= average <=7.5 :
-    print(f" the mark is:  {grade}      {grade1}      {grade2}       {grade3}     {grade4}    {average}    :c     ")
-elif 4 <= average <=6 :
-    print(f" the mark is:  {grade}      {grade1}       {grade2}       {grade3}    {grade4}     {average}      :D     ")
-elif 0 <= average <=4 :
-    print(f" the mark is: {grade}        {grade1}       {grade2}       {grade3}     {grade4}   {average}      :Zero     ")
+if 9 <= average  <= 10 :
+        grade = "A"
+elif 7.5 <= average < 9 :
+         grade = "B"
+elif 6<= average < 7.5 :
+        grade = "C"
+elif 4 <= average < 6 :
+        grade ="D"
+elif 0 <= average < 4 :
+        grade = "Zero"
 else:
-    print(" the maximum mark is 10 you entered invalid number")
-print_line(delimiter="*")
+    print("you enter invalid number")
+
+calculate_grade(grade1,grade2)
+
+print(f"\nThe average grade is : {average}" )
+print(f"\nthe letter_grade is:{grade:}")
+print_line("*")
+
