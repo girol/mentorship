3e9638a3b2244eb7779eb1161c74ca4a458c2e25
André Luiz Girol <andregirol@gmail.com>
2024-09-04 10:47:14 -0300

    Consertando o algoritmo
    - move código para funções
    - conserta cálculo de média
    - mostra resultado "aprovado" e "reprovado"

--- challenges/grades/main.py
+++ challenges/grades/main.py
@@ -1,35 +1,47 @@
-
-
-"""Write a program that reads the two partial grades obtained by a student in a subject over 
-the course of a semester, and calculates their average. 
+"""Write a program that reads the two partial grades obtained by a student in a subject over
+the course of a semester, and calculates their average.
 The assignment of grades follows the table below:"""
 
 
-def calculate_grade(grade1, grade2):
-    grade = grade1 + grade2 /2
+def calculate_average(grade1, grade2):
+    average = (grade1 + grade2) / 2
+    return average
+
+def calculate_concept(average):
+    if 9 <= average <= 10:
+        concept = "A"
+    elif 7.5 <= average < 9:
+        concept = "B"
+    elif 6 <= average < 7.5:
+        concept = "C"
+    elif 4 <= average < 6:
+        concept = "D"
+    else:
+        concept = "E"
+    return concept
+
 def print_line(delimiter="*"):
     print(delimiter * 50)
-print_line("*")
-grade1 = float(input("\nPlease enter the first grade from zero to ten>>:"))
-grade2 = float(input("\nPlease enter the second grade  from zero to ten>>:"))
-average = (grade1 + grade2) /2
 
-if 9 <= average  <= 10 :
-        grade = "A"
-elif 7.5 <= average < 9 :
-         grade = "B"
-elif 6<= average < 7.5 :
-        grade = "C"
-elif 4 <= average < 6 :
-        grade ="D"
-elif 0 <= average < 4 :
-        grade = "Zero"
-else:
-    print("you enter invalid number")
 
-calculate_grade(grade1,grade2)
+def run():
+    print_line("*")
 
-print(f"\nThe average grade is : {average}" )
-print(f"\nthe letter_grade is:{grade:}")
-print_line("*")
+    grade1 = float(input("\nPlease enter the first grade from 0 to 10 >>: "))
+    grade2 = float(input("\nPlease enter the second grade from 0 to 10 >>: "))
 
+    average = calculate_average(grade1, grade2)
+    concept = calculate_concept(average)
+
+    print(f"\nThe average is: {average}")
+    print(f"\nthe concept is: {concept}")
+    print_line("*")
+    print(f"\n RESULT: \n")
+
+    if concept in ["A", "B", "C"]:
+        print("APPROVED! :) ")
+    else:
+        print("FAILED :(")
+
+if __name__ == "__main__":
+    run()
