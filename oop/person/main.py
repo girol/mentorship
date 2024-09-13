208a2021e2a3c064bd77b3fe77c2647c2ef04ae3
Aalim Jannaty <aalim.jannaty@gmail.com>
2024-09-13 14:32:39 -0300

    created a program which shows the details of one persons

---
+++ oop/person/main.py
@@ -0,0 +1,11 @@
+from person import Person
+
+Qurban = Person("Qurban", 20, 80,175)
+print(Qurban)
+Qurban.get_older( )
+print(Qurban)
+Qurban.gain_weight(5)
+print(Qurban)
+Qurban.lose_weight(3)
+print(Qurban)
+     
