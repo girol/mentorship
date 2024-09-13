4b3fa6dd76543b135dbe9105a19bb0db6a91ecff
André Luiz Girol <andregirol@gmail.com>
2024-09-13 15:58:47 -0300

    Ajusta formatação do arquivo

--- oop/person/main.py
+++ oop/person/main.py
@@ -1,11 +1,13 @@
 from person import Person
 
-Qurban = Person("Qurban", 20, 80,175)
-print(Qurban)
-Qurban.get_older( )
-print(Qurban)
-Qurban.gain_weight(5)
-print(Qurban)
-Qurban.lose_weight(3)
-print(Qurban)
-     
+qurban = Person("Qurban", 20, 80, 175)
+print(qurban)
+
+qurban.get_older()
+print(qurban)
+
+qurban.gain_weight(5)
+print(qurban)
+
+qurban.lose_weight(3)
+print(qurban)
