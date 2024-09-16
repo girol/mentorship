752b2565146ceae8d958bacac101da2807d656bf
André Luiz Girol <andregirol@gmail.com>
2024-09-16 11:02:28 -0300

    Fix método change_area e formatação de prints

--- oop/square/main.py
+++ oop/square/main.py
@@ -1,10 +1,10 @@
 from classes import Square
-my_square = Square (4)
 
-print(f"current side:{my_square.get_side()}")
+my_square = Square(4)
 
-print(f"Area:{my_square.calculate_area()}")
+print(f"Current side: {my_square.get_side()}")
+print(f"Area: {my_square.calculate_area()}")
 
-Square.change_side(6)
-print(f"new side :{my_square.get_side()}")
-print(f"new area:{my_square.calculate_area()}")
+my_square.change_side(6)
+print(f"New side: {my_square.get_side()}")
+print(f"New area: {my_square.calculate_area()}")
